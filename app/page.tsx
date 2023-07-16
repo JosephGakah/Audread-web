93b922f783e0d0ad4c839786c64b348d98e0b9b9
JosephGakah <joegakah@gmail.com>
2023-07-16 11:53:37 +0300

    Featured section and pages changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from 'next/image'
+import Header from './components/header'
+import Featured from './components/featured'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <>
+      <div className="loading-wrapper"></div>
+      <div className="header-wrapper"></div>
+      <div className="page-wrapper">
+        <section className="pt-24 pb-24">
+        <div className="container">
+            <div className="text-3xl md:text-5xl text-center text-title mt-6">
+                Featured and seen in
+            </div>
+        </div>
+    </section>
       </div>
-    </main>
+      <div className="footer-wrapper"></div>
+    </>
   )
 }
